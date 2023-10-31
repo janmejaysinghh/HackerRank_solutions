acd2d97ca1202e31283bd370373bb23a3972473d
thejanmejaysingh <95363621+janmejaysinghh@users.noreply.github.com>
2023-10-31 10:56:23 +0530

    Solution.java

--- Java/Strings/Pattern Syntax Checker/Solution.java
+++ Java/Strings/Pattern Syntax Checker/Solution.java
@@ -6,19 +6,18 @@ import java.util.regex.PatternSyntaxException;
 
 // If a PatternSyntaxException is not thrown by Pattern.compile, the regular expresion is valid
 public class Solution {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int testCases = scan.nextInt();
-        scan.nextLine(); // gets rid of the pesky newline.
-        while (testCases-- > 0) {
-           String pattern = scan.nextLine();
-           try {
-               Pattern.compile(pattern);
-               System.out.println("Valid");
-           } catch (PatternSyntaxException exception) {
-               System.out.println("Invalid");
-           }
-        }
-        scan.close();
+    	Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		// Check if there's a next integer to read
+        while(testCases>0){
+            String pattern = in.nextLine();
+              //Write your code
+              try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
     }
 }
